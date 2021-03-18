d85d7bfe20fb513634d78d7224b88b2907c9b4a2
NellZhuGitHub <3191966257@qq.com>
2021-03-18 23:39:07 +0800

    百度首页自动化

--- pages/baidu_home.py
+++ pages/baidu_home.py
@@ -10,10 +10,38 @@ class BaiduHomePage(BasePage):
         # 调用基类的构造方法
         BasePage.__init__()
 
-        # 定位元素
+        # 定位元素""
+        self.top_left_news = self.get_conf_element("baiduHomePage","top_left_news")
+        self.top_left_hao123 = self.get_conf_element("baiduHomePage","top_left_hao123")
+        self.top_left_map = self.get_conf_element("baiduHomePage","top_left_map")
+        self.top_left_live = self.get_conf_element("baiduHomePage","top_left_live")
+        self.top_left_haokan = self.get_conf_element("baiduHomePage","top_left_haokan")
+        self.top_left_tieba = self.get_conf_element("baiduHomePage","top_left_tieba")
+        self.top_left_xueshu= self.get_conf_element("baiduHomePage", "top_left_xueshu")
         self.home_setting_dropdown = self.get_conf_element("baiduHomePage", "home_setting_dropdown")
 
     # 获取元素对象
+    def get_top_left_news(self):
+        return self.get_element(self.top_left_news)
+
+    def get_top_left_hao123(self):
+        return self.get_element(self.top_left_hao123)
+
+    def get_top_left_map(self):
+        return self.get_element(self.top_left_map)
+
+    def get_top_left_live(self):
+        return self.get_element(self.top_left_live)
+
+    def get_top_left_haokan(self):
+        return self.get_element(self.top_left_haokan)
+
+    def get_top_left_tieba(self):
+        return self.get_element(self.top_left_tieba)
+
+    def get_top_left_xueshu(self):
+        return self.get_element(self.top_left_xueshu)
+
     def get_setting_dropdown(self):
         return self.get_element(self.home_setting_dropdown)
 
@@ -27,9 +55,30 @@ class BaiduHomeHandle(BaseHandle):
         # 调用对象库层类
         self.hp = BaiduHomePage()
 
+    def click_top_left_news(self):
+        self.click_element(self.hp.get_top_left_news())
+
+    def click_top_left_hao123(self):
+        self.click_element(self.hp.get_top_left_hao123())
+
+    def click_top_left_map(self):
+        self.click_element(self.hp.get_top_left_map())
+
+    def click_top_left_live(self):
+        self.click_element(self.hp.get_top_left_live())
+
+    def click_top_left_haokan(self):
+        self.click_element(self.hp.get_top_left_haokan())
+
+    def click_top_left_tieba(self):
+        self.click_element(self.hp.get_top_left_tieba())
+
+    def click_top_left_xueshu(self):
+        self.click_element(self.hp.get_top_left_xueshu())
+
     # 鼠标悬浮操作
     def hover_setting_dropdown(self):
         self.move_to_element(self.hp.home_setting_dropdown)
-        s = Select(self.hp.home_setting_dropdown)
+        # s = Select(self.hp.home_setting_dropdown)
 
-    def
+
